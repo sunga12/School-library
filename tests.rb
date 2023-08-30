0b1fc3a273d7dc97201ee74e2f1b335e2812be57
Sunga Thawethe <sungabt@gmail.com>
2023-08-30 21:27:12 +0200

    Update according to 1st review

--- tests.rb
+++ tests.rb
@@ -5,17 +5,12 @@ require './trimmer_decorator'
 require './book'
 require './rental'
 
-# person = Person.new(22, 'maximilianus')
-# p person.correct_name
-# capitalized_person = CapitalizeDecorator.new(person)
-# puts capitalized_person.correct_name
-# capitalized_trimmed_person = TrimmerDecorator.new(capitalized_person)
-# puts capitalized_trimmed_person.correct_name
-
-# book1 = book 1
-# book2 = book 2
-# rental = rental
-# person = person
+person = Person.new(22, 'maximilianus')
+p person.correct_name
+capitalized_person = CapitalizeDecorator.new(person)
+puts capitalized_person.correct_name
+capitalized_trimmed_person = TrimmerDecorator.new(capitalized_person)
+puts capitalized_trimmed_person.correct_name
 
 book1 = Book.new('Emery black', 'Jax')
 book2 = Book.new('Harry Potter', 'JKRowling')
@@ -30,7 +25,6 @@ p "book1 rentals #{book1.rentals.count}"
 book1.rentals.map(&:date)
 p "person john #{person_john.rentals.count}"
 p "person maria #{person_maria.rentals.count}"
-# person_maria.rentals.map { |rental| p rental.book.title}
 
 Rental.new('2017-11-11', person_john, book2)
 Rental.new('2017-10-10', person_maria, book2)
@@ -38,6 +32,4 @@ Rental.new('2017-10-10', person_maria, book2)
 p "book2 rentals #{book2.rentals.count}"
 book2.rentals.map(&:date)
 p "person john #{person_john.rentals.count}"
-# person_john.rentals.map { |rental| rental.book.author }
 p "person maria #{person_maria.rentals.count}"
-# person_maria.rentals.map { |rental| rental.book.author }
